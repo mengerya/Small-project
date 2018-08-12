55f7fce03e1271b6a57c29c01540a7df97d1709f
mengerya <30553694+mengerya@users.noreply.github.com>
2018-08-12 21:02:13 +0800

    Update select.c

--- Http_server/wwwroot/Mysql/select.c
+++ Http_server/wwwroot/Mysql/select.c
@@ -1,78 +1,54 @@
 #include<stdio.h>
 #include<mysql/mysql.h>
 #include"cgi_base.hpp"
+/*192.168.17.129:9090/mysql/mysql/select.html?num=41509030127&password=1272727*/
 
 int main(){
-  //0.获取 query_string
-  char buf[1024*4]={0};
-  if(GetQueryString(buf)<0){
-    fprintf(stderr,"[CGI]GetQueryString error\n");
+	//1.获取到query_string
+  char query_string[1024*4]={0};
+  int ret = GetQueryString(query_string);
+  if(ret == 0){
+    fprintf(stderr,"GetQueryString error!\n");
     return 1;
   }
-  /////////////////////////////////////////////////////////////
-  // 接下来进行有关数据库的操作
-  // 直接把数据库的表中的数据都给查出来
-  // mysql api  使用的一般思路：
-  // 1.连接到数据库
-  // 2.拼装sql语句
-  // 3.把SQL语句发送到服务器
-  // 4.读取并遍历服务器返回的结果
-  // 5.断开连接
-  /////////////////////////////////////////////////////////////
-  
-  
-  //1.连接数据库
+  //query_string 形如num=41509030127&password=1272727
+  long int num=0;
+  char password[24]={0};
+  sscanf(query_string,"num=%ld&password=%s",&num,password);
+  //将用户名与密码在数据库中进行查找配对，若成功则进入学生管理系统，若失败则返回失败页面
+  //2.连接数据库
   //创建MYSQL句柄
   MYSQL* connect_fd = mysql_init(NULL);
   //连接句柄和数据库
-  MYSQL* connect_ret=mysql_real_connect(connect_fd,"127.0.0.1","root","maomeng0501","test1",3306,NULL,0);
+  MYSQL* connect_ret = mysql_real_connect(connect_fd,"127.0.0.1","root","maomeng0501","school",3306,NULL,0);
 
-  if(NULL == connect_ret){
-    fprintf(stderr,"mysql connect failed\n");
+  if(NULL== connect_ret){
+    fprintf(stderr,"mysql_real_connect failed!\n");
     return 1;
   }
   fprintf(stderr,"mysql connect ok!\n");
 
-  //2.拼装SQL语句
-  const char* sql = "select * from TestTable";
+  //3.拼装SQL语句
+  char sql[1024*4]={0};
+  sprintf(sql,"select password from teacher where num='%ld'",num);
 
-  //3.把SQL语句发送到服务器上
-  int ret = mysql_query(connect_fd,sql);
-  if(ret<0){
-    fprintf(stderr,"mysql_query failed!%s\n",sql);
+  //4.将SQL语句返回给服务器
+  ret = mysql_query(connect_fd,sql);
+  if(ret != 0){
+    printf("用户不存在，请重新刷新页面！\n");
+    mysql_close(connect_fd);
     return 1;
   }
-
-  //4.读取并遍历服务器返回的结果
+  //5.读取服务器返回的结构
   MYSQL_RES* result = mysql_store_result(connect_fd);
   if(NULL == result){
-    fprintf(stderr,"[CGI]mysql_store_result failed!\n");
+    fprintf(stderr,"mysql_store_result failed!\n");
+    mysql_close(connect_fd);
     return 1;
   }
-  //a)获取到表里面有几行几列
-  int rows=mysql_num_rows(result);
-  int fields=mysql_num_fields(result);
-  //b)获取到结果集合的表结构
-  MYSQL_FIELD* field = mysql_fetch_field(result);
-  while(field != NULL){
-    printf("%s\t",field->name);
-    field = mysql_fetch_field(result);
-  }
-  printf("<br>");
-  //c)获取到每个元素的具体值
-  int i=0;
-  for(;i<rows;++i){
-    MYSQL_ROW row=mysql_fetch_row(result);
-    int j=0;
-    for(;j<fields;++j){
-      printf("%s\t",row[j]);
-    }
-    printf("<br>");
-  }
-  printf("<br>");
+
+  //6.获取到服务器中对应用户名所对应的密码
   
-  //5.断开连接
-  mysql_close(connect_fd);
-  return 0;
-  
+
+	return 0;
 }
