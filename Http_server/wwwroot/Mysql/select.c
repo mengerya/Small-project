1ebcf74254f34754d0af499e4a02ec6a94c3e306
mengerya <30553694+mengerya@users.noreply.github.com>
2018-08-14 22:45:39 +0800

    Update select.c

--- Http_server/wwwroot/Mysql/select.c
+++ Http_server/wwwroot/Mysql/select.c
@@ -1,7 +1,35 @@
 #include<stdio.h>
+#include<fcntl.h>
 #include<mysql/mysql.h>
 #include"cgi_base.hpp"
 /*192.168.17.129:9090/mysql/mysql/select.html?num=41509030127&password=1272727*/
+
+//写成功的网页
+void RenderHtml(unsigned long long id){
+  //1.打开 select.html文件
+  int fd = open("./wwwroot/mysql/select.html",O_RDONLY);
+  if(fd<0){
+    fprintf(stderr,"open failed!\n");
+    return;
+  }
+  //2.逐个字节读取文件中的内容
+  char c='\0';
+  while(read(fd,&c,1)>0){
+    //判定当前位置是否为特殊字符位置
+    //如果是，在该位置写入要填写的信息
+    if(c == '\1'){
+      printf("%llu",id);
+      fflush(stdout);
+    }
+    else{
+      //将读到的当前字符直接写入
+      write(1,&c,1);
+    }
+  }
+  //3.关闭文件
+  close(fd);
+}
+
 
 int main(){
 	//1.获取到query_string
@@ -12,9 +40,14 @@ int main(){
     return 1;
   }
   //query_string 形如num=41509030127&password=1272727
-  long int num=0;
+  unsigned long long id=0;
   char password[24]={0};
-  sscanf(query_string,"num=%ld&password=%s",&num,password);
+  sscanf(query_string,"num=%llu&password=%s",&id,password);
+
+  /***********DEBUG**************/
+  fprintf(stderr,"num=%llu,password=%s\n",id,password);
+  /******************************/
+  
   //将用户名与密码在数据库中进行查找配对，若成功则进入学生管理系统，若失败则返回失败页面
   //2.连接数据库
   //创建MYSQL句柄
@@ -30,17 +63,21 @@ int main(){
 
   //3.拼装SQL语句
   char sql[1024*4]={0};
-  sprintf(sql,"select password from teacher where num='%ld'",num);
+  sprintf(sql,"select password from teacher where id = '%llu'",id);
+
+  /***************************DEBUG****************************/
+  fprintf(stderr,"sql语句：%s\n",sql);
+  /************************************************************/
 
   //4.将SQL语句返回给服务器
   ret = mysql_query(connect_fd,sql);
-  if(ret != 0){
-    printf("用户不存在，请重新刷新页面！\n");
+  if(ret < 0){
+    fprintf(stderr,"mysql_query failed!\n");
     mysql_close(connect_fd);
     return 1;
   }
   //5.读取服务器返回的结构
-  MYSQL_RES* result = mysql_store_result(connect_fd);
+  MYSQL_RES * result = mysql_store_result(connect_fd);
   if(NULL == result){
     fprintf(stderr,"mysql_store_result failed!\n");
     mysql_close(connect_fd);
@@ -48,6 +85,31 @@ int main(){
   }
 
   //6.获取到服务器中对应用户名所对应的密码
+  char password_sql[100]={0};
+  //获取到元素的具体值
+  MYSQL_ROW row = mysql_fetch_row(result);
+  if(NULL == row){
+    printf("<html>");
+    printf("<h1>mysql 查询失败，用户不存在，请重新尝试！</h1><br>");
+    printf("</html>");
+    mysql_close(connect_fd);
+    return 1;
+  }
+
+  strcpy(password_sql,row[0]);
+  /************************DEBUG****************************/
+  fprintf(stderr,"password_sql:%s",password_sql);
+  /*********************************************************/
+
+  mysql_close(connect_fd);
+  //判断用户输入密码是否正确
+  if(strcmp(password,password_sql) == 0){
+    RenderHtml(id);
+  }else{
+    printf("<html>");
+    printf("<h1>用户密码输入错误，请重新输入！</h1>");
+    printf("</html>");
+  }
   
 
 	return 0;
