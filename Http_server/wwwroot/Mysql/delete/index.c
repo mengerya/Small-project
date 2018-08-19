d0b4b6c558875d7901e2e075bd4e41f9095ba370
mengerya <30553694+mengerya@users.noreply.github.com>
2018-08-19 23:13:34 +0800

    Create index.c

---
+++ Http_server/wwwroot/Mysql/delete/index.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<mysql/mysql.h>
+#include"cgi_base.hpp"
+
+//写成功的网页
+void RenderHtml(){
+  //1.打开 select.html文件
+  int fd = open("./wwwroot/mysql/delete/index.html",O_RDONLY);
+  if(fd<0){
+    fprintf(stderr,"open failed!\n");
+    return;
+  }
+  //2.逐个字节读取文件中的内容
+  char c='\0';
+  while(read(fd,&c,1)>0){
+      //将读到的当前字符直接写入
+      write(1,&c,1);
+  }
+  //3.关闭文件
+  close(fd);
+}
+
+
+int main(){
+   RenderHtml();
+	return 0;
+}
